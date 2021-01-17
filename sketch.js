80c6d978cd182560c2eca6b5f4fccfbb77306e38
santhiyaG86 <74539840+santhiyaG86@users.noreply.github.com>
2021-01-17 11:21:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,42 +1,55 @@
-var thickness, wall;
-var bullet,speed,weight;
+
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1600,400);
-  thickness = random(22,83);
-  speed = random(223,321);
-  weight = random(30,52);
-  bullet = createSprite(50,200,50,50);
-  bullet.shapeColor = "cyan";
-  bullet.velocityX = speed;
-  wall = createSprite(1000,200,thickness,height/2);
-  wall.shapeColor = "white";
-}
+  createCanvas(1600, 400);
 
+  speed=random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+  bullet=createSprite(50, 200, 50,5);  
+  bullet.velocityX = speed;
+  bullet.shapeColor=color(255);
+  wall=createSprite(1200, 200, thickness, height/2);  
+  wall.shapeColor=color(230,230,230);
+  //wall.shapeColor=color(80,80,80)
+}
 
 function draw() {
   background(0);  
-  //console.log(wall.x-bullet.x)
-  if (hasCollided(lbullet,lwall)){
-    bullet.velocityX = 0;
-    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
-    if (damage>10){
-      wall.shapecolor = "red";
-    }
-    if (damage<10){
-      wall.shapeColor = "green";
-    }
-  }
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
-  function hasCollided(lbullet,lwall){
-    bulletRightEdge = lbullet.x+lbullet.width;
-    wallLeftEdge = lwall.x;
-    if(bulletRightEdge>=wallLeftEdge)
-    {
-     return true;
-    } 
-      return false;
-    }
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
 
   drawSprites();
+ 
+}
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
+}
+
+
